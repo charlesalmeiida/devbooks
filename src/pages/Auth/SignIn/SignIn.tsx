dcfeaac486d4dee9688bf5d065bf8ae522182f22
charlesalmeiida <charlesalmeida844@gmail.com>
2024-10-18 09:41:00 -0300

    feat: create link component

--- src/pages/Auth/SignIn/SignIn.tsx
+++ src/pages/Auth/SignIn/SignIn.tsx
@@ -1,13 +1,20 @@
 import { Button } from '../../../components/Button'
 import { Input } from '../../../components/Input'
+import { Link } from '../../../components/Link'
 
 export function SighnIn() {
   return (
-    <>
+    <div style={{ width: 600 }}>
       <h1>Faça seu login</h1>
+      <p>
+        Não tem uma conta?{' '}
+        <Link to={'/'} color="primary">
+          Cadastre-se
+        </Link>
+      </p>
       <Input id="email" label="Email" type="email" />
       <Input id="password" label="Senha" type="password" />
       <Button>Entrar</Button>
-    </>
+    </div>
   )
 }
