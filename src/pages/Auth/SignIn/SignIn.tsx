bb81e6022ba5003d817329e2ce349530528aae20
charlesalmeiida <charlesalmeida844@gmail.com>
2024-10-19 10:30:24 -0300

    feat: styling the signin page

--- src/pages/Auth/SignIn/SignIn.tsx
+++ src/pages/Auth/SignIn/SignIn.tsx
@@ -2,21 +2,44 @@ import { Button } from '../../../components/Button'
 import { Input } from '../../../components/Input'
 import { Link } from '../../../components/Link'
 import { Logo } from '../../../components/Logo'
+import {
+  Container,
+  FormContainer,
+  Heading,
+  InputContainer,
+  LogoContainer
+} from '../Auth.styles'
 
 export function SighnIn() {
   return (
-    <div style={{ width: 600 }}>
-      <Logo />
-      <h1>Faça seu login</h1>
-      <p>
-        Não tem uma conta?{' '}
-        <Link to={'/'} color="primary">
-          Cadastre-se
-        </Link>
-      </p>
-      <Input id="email" label="Email" type="email" />
-      <Input id="password" label="Senha" type="password" />
-      <Button>Entrar</Button>
-    </div>
+    <Container>
+      <FormContainer>
+        <div style={{ width: 600 }}>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
+
+          <Heading>
+            <h1>Faça seu login</h1>
+            <p>
+              Não tem uma conta?{' '}
+              <Link to={'/'} color="secondary">
+                Cadastre-se
+              </Link>
+            </p>
+          </Heading>
+
+          <InputContainer>
+            <Input id="email" label="Email" type="email" />
+          </InputContainer>
+
+          <InputContainer>
+            <Input id="password" label="Senha" type="password" />
+          </InputContainer>
+
+          <Button fullWidth>Entrar</Button>
+        </div>
+      </FormContainer>
+    </Container>
   )
 }
